a9e5884904b3b2a2c1c7af4626fabfbdcf61b2eb
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2019-12-17 09:29:44 +0100

    Set default port to 3001

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    EXAMPLE_URL : 'http://localhost:8080/examples/',
+    EXAMPLE_URL : 'http://localhost:3001/examples/',
     SHACL_FILE_URL:  'https://data.vlaanderen.be/shacl/',
     FILE_CREATION_URL: 'http://localhost:3002/validation'
 }
