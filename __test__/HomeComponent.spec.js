1d0bc33f3f1fd09c24533ad53dcaba10d36e40b4
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-06-19 15:00:38 +0200

    Updated test for HomeComponent

--- __test__/HomeComponent.spec.js
+++ __test__/HomeComponent.spec.js
@@ -1,7 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import HomeComponent from "../src/components/HomeComponent.vue";
-import { describe, expect, jest} from "@jest/globals";
-import Vue from 'vue';
+import { describe, expect} from "@jest/globals";
 
 const wrapper = shallowMount(HomeComponent, {
     stubs: [
@@ -22,8 +21,7 @@ const wrapper = shallowMount(HomeComponent, {
 
 describe('HomeComponent', () => {
 
-    // Evaluate the results of functions in
-    // the raw component options
+    // Evaluate the results of functions in the raw component options
     test('it sets the correct default data', () => {
        expect(typeof HomeComponent.data).toBe('function');
        const defaultData = HomeComponent.data();
@@ -54,10 +52,26 @@ describe('HomeComponent', () => {
 
     });
 
-    test('it should fetch URLs containing JSON(-LD) data', () => {
+    //TODO
+    test('it should fetch URLs containing JSON(-LD) data', async () => {
+        /*const data = {
+            "@context": "http://schema.org/",
+            "@type": "Person",
+            "name": "Jane Doe",
+            "jobTitle": "Professor",
+            "telephone": "(425) 123-4567",
+            "url": "http://www.janedoe.com"
+        };
+
+        // Setting a valid test sample that contains JSON-LD
+        wrapper.setData({documentURL: 'https://gist.githubusercontent.com/ddvlanck/b556f13e5085f0845571046454617765/raw/b9a5e9636114dbe9fb8b24ed3ac3e1386fed7e0d/test-example.jsonld'})
+        const fetchButton = wrapper.find('#fetchButton');
+        await fetchButton.trigger('click');
+        await expect(wrapper.vm.$data.documentData).resolves.toEqual(data);*/
 
     });
 
+    //TODO
     test('it should throw an error when fetching a URL that does not contain JSON', () => {
     });
 })
