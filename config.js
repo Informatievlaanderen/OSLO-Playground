6611c0b26a7070742d35bdd72a93e4c24558a2dd
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2021-07-06 15:27:39 +0200

    Update configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     EXAMPLE_URL : 'http://localhost:8080/examples/',
     SHACL_VALIDATION:  'https://test.data.vlaanderen.be/shacl-validator-backend/shacl/applicatieprofielen/api/validate',
-    VALIDATOR_BACKEND_CONFIG: 'https://raw.githubusercontent.com/Informatievlaanderen/OSLO-Validator-EU/dev/Validator-Backend/resources/applicatieprofielen/config.properties'
+    VALIDATOR_BACKEND_CONFIG: 'https://github.com/Informatievlaanderen/OSLO-Validator-EU/blob/dev/packages/oslo-itb-rdf-validator/resources/applicatieprofielen/config.properties'
 }
